1768a4160060c1bdba393a4b2239e256bb013b13
= <=>
2020-11-11 11:26:07 +0000

    cors fixed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -8,6 +9,7 @@ const notesRouter = require("./routes/notes");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
